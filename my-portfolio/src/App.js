bb1020c3e7f347d870e39d2798eb7eddc248f49d
John W <john.weaver94@utexas.edu>
2023-06-19 08:49:36 -0500

    added react folders, and edited them slightly

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Portfolio from './components/Portfolio';
+
+
+function App() {
+  return (
+    <Portfolio />
+  );
+}
+
+export default App;
